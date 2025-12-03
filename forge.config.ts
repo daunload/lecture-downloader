a4d8c32c98d3543afdb76b4cec8cf856e93a1f38
daunload <daun4799@gmail.com>
2025-12-03 15:02:04 +0900

    publisher-github 추가

--- forge.config.ts
+++ forge.config.ts
@@ -1,10 +1,10 @@
-import type { ForgeConfig } from '@electron-forge/shared-types';
-import { MakerSquirrel } from '@electron-forge/maker-squirrel';
-import { MakerZIP } from '@electron-forge/maker-zip';
 import { MakerDeb } from '@electron-forge/maker-deb';
 import { MakerRpm } from '@electron-forge/maker-rpm';
-import { VitePlugin } from '@electron-forge/plugin-vite';
+import { MakerSquirrel } from '@electron-forge/maker-squirrel';
+import { MakerZIP } from '@electron-forge/maker-zip';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
+import { VitePlugin } from '@electron-forge/plugin-vite';
+import type { ForgeConfig } from '@electron-forge/shared-types';
 import { FuseV1Options, FuseVersion } from '@electron/fuses';
 
 const config: ForgeConfig = {
@@ -53,6 +53,19 @@ const config: ForgeConfig = {
 			[FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
 			[FuseV1Options.OnlyLoadAppFromAsar]: true,
 		}),
+	],
+	publishers: [
+		{
+			name: '@electron-forge/publisher-github',
+			config: {
+				authToken: process.env.GH_PAT,
+				repository: {
+					owner: 'daunload',
+					name: 'lecture-downloader',
+				},
+				prerelease: true,
+			},
+		},
 	],
 };
 
